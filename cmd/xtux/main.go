7a71da3905e3cc74e99bdcd38774cd2d1674fd34
NachiketKandari <nachiket.kandari@gmail.com>
2026-09-13 11:30:00 +0530

    xtux: scan/ir inspection CLI

---
+++ cmd/xtux/main.go
@@ -0,0 +1,69 @@
+// Command xtux inspects the experimental scanner/IR layer: it dumps
+// scanner facts or extracted IR as JSON for a file or directory.
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"tux-to-any/internal/ir"
+	"tux-to-any/internal/tsscan"
+)
+
+func main() {
+	frag := flag.Bool("fragment", false, "force the fragment rubric (file mode)")
+	flag.Parse()
+	if flag.NArg() < 2 {
+		fmt.Fprintln(os.Stderr, "usage: xtux scan|ir <file|dir> [-fragment]")
+		os.Exit(2)
+	}
+	cmd, target := flag.Arg(0), flag.Arg(1)
+	switch cmd {
+	case "scan":
+		facts, err := tsscan.ScanFile(target)
+		if err != nil {
+			fail(err)
+		}
+		dump(facts)
+	case "ir":
+		info, err := os.Stat(target)
+		if err != nil {
+			fail(err)
+		}
+		if info.IsDir() {
+			files, err := ir.ExtractDir(target)
+			if err != nil {
+				fail(err)
+			}
+			for _, f := range files {
+				dump(f)
+			}
+			return
+		}
+		opts := ir.DefaultOptions()
+		opts.ForceFragment = *frag
+		f, err := ir.ExtractFileOpts(target, opts)
+		if err != nil {
+			fail(err)
+		}
+		dump(f)
+	default:
+		fmt.Fprintln(os.Stderr, "unknown command:", cmd)
+		os.Exit(2)
+	}
+}
+
+func dump(v any) {
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(v); err != nil {
+		fail(err)
+	}
+}
+
+func fail(err error) {
+	fmt.Fprintln(os.Stderr, "xtux:", err)
+	os.Exit(1)
+}
